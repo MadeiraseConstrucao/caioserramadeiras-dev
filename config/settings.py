2f0c878d6a2aaf23dc4dea09282fb64f8aa1f43b
MadeiraseConstrucao <marketingserramadeiras@gmail.com>
2026-05-12 17:31:49 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,28 @@
-
-APP_TITLE = "Consulta Pangeia"
+APP_TITLE = "Utilitários Madeiras e Construção"
 
 LOGO_PATH = "logo.png"
 
+# ==========================================
+# PÁGINAS
+# ==========================================
+
 PAGINA_MENU = "menu"
+
+# ==========================================
+# CONSULTA PANGEIA
+# ==========================================
+
 PAGINA_CLIENTES = "clientes"
 PAGINA_PRODUTOS = "produtos"
+
+# ==========================================
+# PORTAS
+# ==========================================
+
+PAGINA_ORIENTACAO_PORTA = "orientacao_porta"
+
+# ==========================================
+# ENTREGAS
+# ==========================================
+
+PAGINA_ENTREGAS = "entregas"
