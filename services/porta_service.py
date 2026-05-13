36e6dd2e946295fef47686308bee3ac9d87f457b
MadeiraseConstrucao <marketingserramadeiras@gmail.com>
2026-05-13 13:43:04 -0300

    Update webhook URL in enviar_pedido_porta function

--- services/porta_service.py
+++ services/porta_service.py
@@ -7,7 +7,7 @@ import requests
 
 def enviar_pedido_porta(dados):
 
-    WEBHOOK_URL = "COLOCAR_URL_APPS_SCRIPT"
+    WEBHOOK_URL = "https://script.google.com/u/0/home/projects/1oPNNDHDgHlb5FpqLUvNb5zwhVH-6zVpaqYLtfLMKpZq9EbPRz8aDB55x/edit"
 
     response = requests.post(
         WEBHOOK_URL,
