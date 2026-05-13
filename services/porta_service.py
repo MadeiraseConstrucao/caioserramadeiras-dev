1b30d2f9855d6234549039397e796e5203c55a10
MadeiraseConstrucao <marketingserramadeiras@gmail.com>
2026-05-13 14:42:26 -0300

    Update porta_service.py

--- services/porta_service.py
+++ services/porta_service.py
@@ -3,7 +3,7 @@ import requests
 
 def enviar_pedido_porta(dados):
 
-    WEBHOOK_URL = "https://script.google.com/macros/library/d/1oPNNDHDgHlb5FpqLUvNb5zwhVH-6zVpaqYLtfLMKpZq9EbPRz8aDB55x/3"
+    WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzeadb3308jmUg1YBHSGB2XEA-fJ8JKNMKQ2QRe3hvqL5EDSPUDM6rU4LvWMJPQF2UwQQ/exec"
 
     response = requests.post(
         WEBHOOK_URL,
